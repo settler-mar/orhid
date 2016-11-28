79f4ed4b13b4e1a93e756b9beafa12b5219fcd77
Max Matiukhin <matuhinmax@mail.ru>
2016-11-28 10:48:06 +0200

    Вернул на место app assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -9,10 +9,6 @@ namespace app\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
@@ -24,6 +20,5 @@ class AppAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
 }
