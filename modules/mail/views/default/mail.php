c69f6fb4d6cdc0bb6e4f65fc6e3cc5a3266056ee
Max Matiukhin <matuhinmax@mail.ru>
2017-04-03 22:24:58 +0300

    Сделал вывод истории переписки

--- modules/mail/views/default/mail.php
+++ modules/mail/views/default/mail.php
@@ -12,19 +12,19 @@
 
   $this->title = 'Mail';
   $this->params['breadcrumbs'][] = $this->title;
-  $this->params['hide_title']=true;
+  //$this->params['hide_title']=true;
 
   $old=date('Y',time()-$user->profile['birthday'])-1970;
   $online = (time()-($user->last_online)<User::MAX_ONLINE_TIME);
 ?>
 
-<div class="content">
   <?php $form = ActiveForm::begin([id=>"msg_form"]); ?>
 
   <?=yii\imperavi\Widget::widget([
     // You can either use it for model attribute
     'model' => $model,
     'attribute' => 'message',
+    'id'=>"message",
 
     // Some options, see http://imperavi.com/redactor/docs/
     'options' => [
@@ -35,13 +35,18 @@
         'unorderedlist',
         'orderedlist'
       ],
-      /*'formatting'=> ['p','blockquote'],
-      'formattingAdd'=>[
-        "red-p-add"=>[
-          title=> 'Red Block Add',
-		      args=> ['p', 'class','red']
-	      ],
-      ]*/
+      'pasteLinks'=>false,
+      'pasteLinkTarget'=>false,
+      'pasteBlockTags'=>[],
+      'pasteInlineTags'=>[],
+      'pasteImages'=>false,
+      'dragImageUpload'=>false,
+      'clipboardImageUpload'=>false,
+      'multipleImageUpload'=>false,
+      'imageCaption'=>false,
+      'imageResizable'=>true,
+      'removeComments'=>false,
+
     ],
     'plugins'=> [
       'inlinestyle',
@@ -52,35 +57,39 @@
 
   <?php ActiveForm::end(); ?>
 
-  <div class="file_insert_model">
-    <div class="close">X</div>
-    <div class="title">Select photo</div>
-    <div class="photo_list">
+  <div class="file_insert_model_bg">
+    <div class="file_insert_model">
+      <div class="close">X</div>
+      <div class="title">Select photo</div>
+      <div class="photo_list">
 
-    </div>
+      </div>
 
-    <div>
-      <?= FileUpload::widget([
-        'model'=>$fileupload,
-        'attribute' => 'image',
-        'url' => ['/fileupload/default/upload'], // your url, this is just for demo purposes,
-        'options' => ['accept' => 'image/*'],
-        'clientOptions' => [
-          'maxFileSize' => 2000
-        ], // Also, you can specify jQuery-File-Upload events
-      //// see: https://github.com/blueimp/jQuery-File-Upload/wiki/Options#processing-callback-options
-        'clientEvents' => [
-          'fileuploaddone' => 'function(e, data) {
-              console.log(e); console.log(data);
-          }',
-          'fileuploadfail' => 'function(e, data) {
-            console.log(e); console.log(data);
-          }',
-          'fileuploadprocess' => 'function(e, data) {
-            console.log(e); console.log(data);
-          }',
-        ],
-      ]);?>
+      <div>
+        <div>
+          You can upload new file from your PC.
+        </div>
+        <div class="progress_bar">
+          <span></span>
+        </div>
+        <?= FileUpload::widget([
+          'model'=>$fileupload,
+          'attribute' => 'image',
+          'url' => ['/fileupload/default/upload'], // your url, this is just for demo purposes,
+          'options' => ['accept' => 'image/*'],
+          'clientOptions' => [
+            'maxFileSize' => 2000
+          ], // Also, you can specify jQuery-File-Upload events
+        //// see: https://github.com/blueimp/jQuery-File-Upload/wiki/Options#processing-callback-options
+          'clientEvents' => [
+            'fileuploaddone' => 'fileuploaddone',
+            'fileuploadfail' => 'fileuploadfail',
+            'fileuploadprogress' => 'fileuploadprogress',
+            'fileuploadstart' => 'fileuploadstart',
+          ],
+        ]);?>
+      </div>
+      <a class="insert_photo">Insert photo</a>
     </div>
   </div>
 
@@ -89,90 +98,47 @@
 
 
 
-    <div class="in_mes in_mes_new">
-      <div class="in_mes_top">
-        <div class="in_mes_img"><img src="img/man.jpg"></div>
-        <div class="in_mes_tit">
-          <div class="in_mes_name">Vladimir_2017</div>
-          <div class="in_mes_data">25.03.2017</div>
-          <div class="in_mes_us">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-            <!--							Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.			-->
+    <?php
+    $dt=strtotime("today");
+      foreach ($mails as $msg_num =>$mail){
+        $do_css='';
+        if($msg_num>0){
+          $do_css.=' close_message';
+        };
+
+        if($m_id==$mail['user_from']){
+          $u=Yii::$app->user->identity;
+        }else{
+          if($mail['is_read']==0){
+            $do_css.=' in_mes_new';
+          }
+          $u=$user;
+        }
+        ?>
+        <div class="in_mes <?=$do_css;?>">
+          <div class="in_mes_top">
+            <div class="in_mes_img"><img src="<?=$u->getPhoto();?>"></div>
+            <div class="in_mes_tit">
+              <div class="in_mes_name"><?=$u->getFullNick();?></div>
+              <div class="in_mes_data"><?=date($dt>$mail['created_at']?"d.m.Y":'H:i',$mail['created_at'])?></div>
+              <div class="in_mes_us">
+                <span class="glyphicon glyphicon-chevron-up"></span>
+                <span class="anotation">
+                  <?=mb_substr(strip_tags($mail['message']),0,300);?>
+                </span>
+              </div>
+
+            </div>
           </div>
 
-        </div>
-      </div>
-
-      <div class="in_mes_cont" style="display:block;">
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-        <p><strong>Contrary to popular belief, Lorem Ipsum is not simply random text.</strong> It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text</p>
-      </div>
-    </div>
-
-    <div class="in_mes">
-      <div class="in_mes_top">
-        <div class="in_mes_img"><img src="img/women.jpg"></div>
-        <div class="in_mes_tit">
-          <div class="in_mes_name">Irishka_1990</div>
-          <div class="in_mes_data">25.03.2017</div>
-          <div class="in_mes_us">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.
+          <div class="in_mes_cont">
+            <?=$mail['message'];?>
           </div>
         </div>
-      </div>
-
-      <div class="in_mes_cont">
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-        <p><strong>Contrary to popular belief, Lorem Ipsum is not simply random text.</strong> It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text</p>
-      </div>
-    </div>
-
-    <div class="in_mes">
-      <div class="in_mes_top">
-        <div class="in_mes_img"><img src="img/man.jpg"></div>
-        <div class="in_mes_tit">
-          <div class="in_mes_name">Vladimir_2017</div>
-          <div class="in_mes_data">25.03.2017</div>
-          <div class="in_mes_us">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.
-          </div>
-
-        </div>
-      </div>
-
-      <div class="in_mes_cont">
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-        <p><strong>Contrary to popular belief, Lorem Ipsum is not simply random text.</strong> It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text</p>
-      </div>
-    </div>
-
-    <div class="in_mes">
-      <div class="in_mes_top">
-        <div class="in_mes_img"><img src="img/women.jpg"></div>
-        <div class="in_mes_tit">
-          <div class="in_mes_name">Irishka_1990</div>
-          <div class="in_mes_data">25.03.2017</div>
-          <div class="in_mes_us">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.
-          </div>
-        </div>
-      </div>
-
-      <div class="in_mes_cont">
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-        <p><strong>Contrary to popular belief, Lorem Ipsum is not simply random text.</strong> It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from Contrary to popular belief, Lorem Ipsum is not simply random text</p>
-      </div>
-    </div>
-
-
-
-
-
-
+        <?php
+      };
+    ?>
 
     <div class="clear"></div>
 
-
-
   </div>
-</div>
---
+++ web/js/mail.js
@@ -0,0 +1,63 @@
+function fileuploaddone(e, data){
+  $('.progress_bar').hide();
+  $('.fileinput-button').show();
+  $('.progress_bar span').width("0");
+  img=add_file_to_list(data.result.files[0].url);
+  img.click()
+  popup.open({message:"Picture was successfully uploaded.",type:'info',title:"File upload done"})
+}
+function fileuploadfail(e, data){
+  $('.progress_bar').hide();
+  $('.fileinput-button').show();
+  popup.open({message:"Try again later or another image.",type:'err',title:"File upload error"})
+}
+function fileuploadprogress(e, data){
+  p=100*data.loaded/data.total;
+  $('.progress_bar span').width(p+"%");
+}
+function fileuploadstart(e, data){
+  $('.progress_bar').show();
+  $('.progress_bar span').width("0");
+  $('.fileinput-button').hide();
+}
+
+$('.file_insert_model_bg .close').on('click',function(){
+  $('.file_insert_model_bg').hide();
+  $('.file_insert_model_bg .select').removeClass('select');
+});
+$('.file_insert_model_bg').hide();
+
+function add_file_to_list(file){
+  $('.file_insert_model_bg .title').show();
+  img=$("<img/>",{
+    src:file
+  });
+  div=$("<div/>");
+  div.append(img);
+
+  $('.photo_list').append(div);
+  return div;
+}
+$('.photo_list').on('click','div',function(){
+  $('.file_insert_model_bg .select').removeClass('select');
+  $(this).addClass('select');
+});
+
+$('.insert_photo').on('click',function(){
+  img=$('.file_insert_model_bg .select img');
+  if(img.length>0){
+    src=img[0].src;
+    jQuery('#message').redactor('insert.html', "<img src='"+src+"' class='user_img'/>");
+  }
+  $('.file_insert_model_bg .select').removeClass('select');
+  $('.file_insert_model_bg').hide();
+});
+
+$('.in_mes_top').on('click',function(){
+  $this=$(this).parent()
+  if($this.hasClass('close_message')){
+    $this.removeClass('close_message')
+  }else{
+    $this.addClass('close_message')
+  }
+});
