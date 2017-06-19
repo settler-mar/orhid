f3d0b16cb2f9ebd01b2bd1baea2a7087e49bbfd2
eastern1984 <tolikybm@gmail.com>
2017-06-19 12:59:29 +0300

    Работа со динамическими маршрутами2

--- components/staticPagesRules.php
+++ components/staticPagesRules.php
@@ -16,6 +16,8 @@ class staticPagesRules extends Object implements UrlRuleInterface
 {
   public function createUrl($manager, $route, $params)
   {
+    print_r(222);
+    //return 'site/index';
     if ($route === 'car/index') {
       if (isset($params['manufacturer'], $params['model'])) {
         return $params['manufacturer'] . '/' . $params['model'];
@@ -29,11 +31,15 @@ class staticPagesRules extends Object implements UrlRuleInterface
   public function parseRequest($manager, $request)
   {
     $pathInfo = $request->getPathInfo();
+    print_r(111);
+    print_r($pathInfo);
     if (preg_match('%^(\w+)(/(\w+))?$%', $pathInfo, $matches)) {
       // Ищем совпадения $matches[1] и $matches[3]
       // с данными manufacturer и model в базе данных
       // Если нашли, устанавливаем $params['manufacturer'] и/или $params['model']
       // и возвращаем ['car/index', $params]
+      print_r($matches);
+      return ['site/index', ['id'=>7]];
     }
     return false;  // данное правило не применимо
   }
