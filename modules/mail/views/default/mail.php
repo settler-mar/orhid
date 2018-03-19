ab91478692f64a7d7e284808ad360a15d2dae9af
Max Matiukhin <matuhinmax@mail.ru>
2018-03-19 17:39:16 +0200

    Мелкие правкии для валидации почты

--- modules/mail/views/default/mail.php
+++ modules/mail/views/default/mail.php
@@ -1,21 +1,21 @@
 <?php
-  use yii\helpers\Html;
-  use yii\grid\GridView;
-  use app\modules\user\models\User;
-  use app\modules\user\models\Profile;
-  use app\components\timePassed;
-  use app\components\emojione\Input;
-  use yii\widgets\ActiveForm;
-  use yii\web\JsExpression;
-  use dosamigos\fileupload\FileUploadUI;
-  use dosamigos\fileupload\FileUpload;
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\modules\user\models\User;
+use app\modules\user\models\Profile;
+use app\components\timePassed;
+use app\components\emojione\Input;
+use yii\widgets\ActiveForm;
+use yii\web\JsExpression;
+use dosamigos\fileupload\FileUploadUI;
+use dosamigos\fileupload\FileUpload;
 
-  $this->title = 'Mail';
-  $this->params['breadcrumbs'][] = $this->title;
-  //$this->params['hide_title']=true;
+$this->title = 'Mail';
+$this->params['breadcrumbs'][] = $this->title;
+//$this->params['hide_title']=true;
 
-  $old=date('Y',time()-$user->profile['birthday'])-1970;
-  $online = (time()-($user->last_online)<User::MAX_ONLINE_TIME);
+$old=date('Y',time()-$user->profile['birthday'])-1970;
+$online = (time()-($user->last_online)<User::MAX_ONLINE_TIME);
 ?>
 <?php
 if(Yii::$app->user->identity->canIdo($rule_name,true)){
@@ -24,37 +24,37 @@ if(Yii::$app->user->identity->canIdo($rule_name,true)){
 
   <?=yii\imperavi\Widget::widget([
     // You can either use it for model attribute
-    'model' => $model,
-    'attribute' => 'message',
-    'id'=>"message",
+      'model' => $model,
+      'attribute' => 'message',
+      'id'=>"message",
 
     // Some options, see http://imperavi.com/redactor/docs/
-    'options' => [
-      'toolbar' => true,
-      'buttons' => [
-        'bold',
-        'italic',
-        'unorderedlist',
-        'orderedlist'
-      ],
-      'pasteLinks'=>false,
-      'pasteLinkTarget'=>false,
-      'pasteBlockTags'=>[],
-      'pasteInlineTags'=>[],
-      'pasteImages'=>false,
-      'dragImageUpload'=>false,
-      'clipboardImageUpload'=>false,
-      'multipleImageUpload'=>false,
-      'imageCaption'=>false,
-      'imageResizable'=>true,
-      'removeComments'=>false,
+      'options' => [
+          'toolbar' => true,
+          'buttons' => [
+              'bold',
+              'italic',
+              'unorderedlist',
+              'orderedlist'
+          ],
+          'pasteLinks'=>false,
+          'pasteLinkTarget'=>false,
+          'pasteBlockTags'=>[],
+          'pasteInlineTags'=>[],
+          'pasteImages'=>false,
+          'dragImageUpload'=>false,
+          'clipboardImageUpload'=>false,
+          'multipleImageUpload'=>false,
+          'imageCaption'=>false,
+          'imageResizable'=>true,
+          'removeComments'=>false,
 
-    ],
-    'plugins'=> [
-      'inlinestyle',
-      'userimage',
-      'submit',
-    ]
+      ],
+      'plugins'=> [
+          'inlinestyle',
+          'userimage',
+          'submit',
+      ]
   ]);;?>
 
   <?php ActiveForm::end(); ?>
@@ -74,26 +74,26 @@ if(Yii::$app->user->identity->canIdo($rule_name,true)){
           <span></span>
         </div>
         <?= FileUpload::widget([
-          'model'=>$fileupload,
-          'attribute' => 'image',
-          'url' => '/fileupload/default/upload', // your url, this is just for demo purposes,
-          'options' => ['accept' => 'image/*'],
-          'clientOptions' => [
-            'maxFileSize' => 2000
-          ], // Also, you can specify jQuery-File-Upload events
-        //// see: https://github.com/blueimp/jQuery-File-Upload/wiki/Options#processing-callback-options
-          'clientEvents' => [
-            'fileuploaddone' => 'fileuploaddone',
-            'fileuploadfail' => 'fileuploadfail',
-            'fileuploadprogress' => 'fileuploadprogress',
-            'fileuploadstart' => 'fileuploadstart',
-          ],
+            'model'=>$fileupload,
+            'attribute' => 'image',
+            'url' => '/fileupload/default/upload', // your url, this is just for demo purposes,
+            'options' => ['accept' => 'image/*'],
+            'clientOptions' => [
+                'maxFileSize' => 2000
+            ], // Also, you can specify jQuery-File-Upload events
+          //// see: https://github.com/blueimp/jQuery-File-Upload/wiki/Options#processing-callback-options
+            'clientEvents' => [
+                'fileuploaddone' => 'fileuploaddone',
+                'fileuploadfail' => 'fileuploadfail',
+                'fileuploadprogress' => 'fileuploadprogress',
+                'fileuploadstart' => 'fileuploadstart',
+            ],
         ]);?>
       </div>
       <a class="insert_photo">Insert photo</a>
     </div>
   </div>
-<?php
+  <?php
 }else{
   ?>
   <div class="video video_no_money">
@@ -102,71 +102,79 @@ if(Yii::$app->user->identity->canIdo($rule_name,true)){
   <?php
 }
 ?>
-  <div class="into">
-    <!--<div class="title_1"><span>Chat</span></div>-->
+<div class="into">
+  <!--<div class="title_1"><span>Chat</span></div>-->
 
 
 
-    <?php
-    $dt=strtotime("today");
-      foreach ($mails as $msg_num =>$mail){
-        $do_css='';
-        if($msg_num>0){
-          $do_css.=' close_message';
-        };
+  <?php
+  $dt=strtotime("today");
+  foreach ($mails as $msg_num =>$mail){
+    $do_css='';
+    if($msg_num>0){
+      $do_css.=' close_message';
+    };
 
-        if($m_id==$mail['user_from']){
-          $u=Yii::$app->user->identity;
-        }else{
-          if($mail['is_read']==0){
-            $do_css.=' in_mes_new';
-          }
-          $u=$user;
-        }
-        ?>
-        <div class="in_mes <?=$do_css;?>">
-          <div class="in_mes_top">
-            <div class="in_mes_img"><img src="<?=$u->getPhoto();?>"></div>
-            <div class="in_mes_tit">
-              <div class="in_mes_name"><?=$u->getFullNick();?></div>
-              <div class="in_mes_data"><?=date($dt>$mail['created_at']?"d.m.Y":'H:i',$mail['created_at'])?></div>
-              <div class="in_mes_us">
-                <span class="glyphicon glyphicon-chevron-up"></span>
-                <span class="anotation"><?=trim(mb_substr(strip_tags($mail['message']),0,300));?></span>
-              </div>
-
-            </div>
+    if($m_id==$mail['user_from']){
+      $u=Yii::$app->user->identity;
+    }else{
+      if($mail['is_read']==0){
+        $do_css.=' in_mes_new';
+      }
+      $u=$user;
+    }
+    ?>
+    <div class="in_mes <?=$do_css;?>">
+      <div class="in_mes_top">
+        <div class="in_mes_img"><img src="<?=$u->getPhoto();?>"></div>
+        <div class="in_mes_tit">
+          <div class="in_mes_name"><?=$u->getFullNick();?></div>
+          <div class="in_mes_data"><?=date($dt>$mail['created_at']?"d.m.Y":'H:i',$mail['created_at'])?></div>
+          <div class="in_mes_us">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+            <span class="anotation"><?=trim(mb_substr(strip_tags($mail['message']),0,300));?></span>
           </div>
 
-          <div class="in_mes_cont">
-            <?=$mail['message'];?>
-          </div>
         </div>
-        <?php
-      };
-    ?>
+      </div>
 
-    <div class="clear"></div>
-
-  </div>
-
-<?php
-  if ( Yii::$app->session->hasFlash('success')) {
-    ?>
-  <script type='text/javascript'>
-    $(document).ready(function () {
-      popup.open({message: '<?=Yii::$app->session->getFlash('success');?>', type: 'success', time: 10000});
-    });
-  </script>
-<?php
-}
-if ( Yii::$app->session->hasFlash('error')) {
+      <div class="in_mes_cont">
+        <?=$mail['message'];?>
+      </div>
+    </div>
+    <?php
+  };
   ?>
+
+  <div class="clear"></div>
+
+</div>
+
+<?php
+if ( Yii::$app->session->hasFlash('success')) {
+  $msg=Yii::$app->session->getFlash('success');
+  if(!is_array($msg)){$msg=[$msg];};
+  foreach ($msg as $m) {
+    ?>
     <script type='text/javascript'>
       $(document).ready(function () {
-        popup.open({message: '<?=Yii::$app->session->getFlash('error');?>', type: 'err', time: 10000});
+        popup.open({message: '<?=$m;?>', type: 'success', time: 10000});
       });
     </script>
-<?php
+    <?php
+  }
+}
+if ( Yii::$app->session->hasFlash('error')) {
+  $msg=Yii::$app->session->getFlash('error');
+  if(!is_array($msg)){$msg=[$msg];};
+  foreach ($msg as $m) {
+    ?>
+    <script type='text/javascript'>
+      $(document).ready(function () {
+        popup.open({message: '<?=$m;?>', type: 'err', time: 10000});
+      });
+    </script>
+    <?php
+  }
 }
 ?>
